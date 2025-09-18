bfbaf724e8b4acb1db4669821c48a9e4616fb0ed
handrakantnb18 <bhosale.chandrakant1806@gmail.com>
2025-09-18 15:16:23 +0530

    assignment- 152 Implement the app level CRUD operation using multiple components

---
+++ ReactJS/assignment-152/src/userModule/AddUser.jsx
@@ -0,0 +1,11 @@
+
+import EditUser from "./editUser"
+import ShowUser from "./showUser"
+
+export default function AddUser() {
+    return (
+        <>
+            <h2>Add user in components</h2><hr />
+        </>
+    )
+}
---
+++ ReactJS/assignment-152/src/userModule/ShowUser.jsx
@@ -0,0 +1,8 @@
+
+export default function ShowUser() {
+    return(
+        <>
+            <h2> Show users in components</h2> <hr />
+        </>
+    )
+}
