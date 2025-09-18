23fbd26a6a4d36888704e93d66690fced84b36c1
handrakantnb18 <bhosale.chandrakant1806@gmail.com>
2025-09-18 14:50:04 +0530

    assignment- 152 Implement the app level CRUD operation using multiple components

--- ReactJS/assignment-152/src/main.jsx
+++ ReactJS/assignment-152/src/main.jsx
@@ -2,11 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Welcome from './welcome.jsx'
-import UserCount from './UserCount.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <UserCount />
+    <App />
   </StrictMode>,
 )
