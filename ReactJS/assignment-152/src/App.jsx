8237e4cb77c43f5a845473b25562390ccc8563dd
handrakantnb18 <bhosale.chandrakant1806@gmail.com>
2025-09-18 20:59:06 +0530

    assignment 152 Implement CRUD operation app level  using multiple components

--- ReactJS/assignment-152/src/App.jsx
+++ ReactJS/assignment-152/src/App.jsx
@@ -1,15 +1,27 @@
-import AddUser from "./userModule/addUser"
-import EditUser from "./userModule/editUser"
-import ShowUser from "./userModule/showUser"
+import { useState } from "react";
+import AddUser from "./userModule/AddUser";
+import ShowUser from "./userModule/ShowUser";
+import ShowUsers from "./userModule/ShowUser";
 
 export default function App() {
-  
+    const initialUserData = {
+      firstName:"Virat",
+      lastName: "Kohli",
+      age: 35,
+      country: "India"
+    };
+
+    const [userData, setUserData] = useState([initialUserData]);
+
+    const _onAddUserEvent = function(userObj) {
+          userData.push(userObj);
+          setUserData([...userData]);
+    }
   return (
     <>
     <h3>React Js </h3>
-        <AddUser />
-        <ShowUser />
-        <EditUser />
+        <AddUser _onAddUserEvent = {_onAddUserEvent.bind(this)}/>
+        <ShowUser usersData= { userData } />
       </>
   )
         
--- ReactJS/assignment-152/src/userModule/AddUser.jsx
+++ ReactJS/assignment-152/src/userModule/AddUser.jsx
@@ -1,11 +1,53 @@
+import { useRef } from "react"
 
-import EditUser from "./editUser"
-import ShowUser from "./showUser"
+export default function AddUser(props) {
 
-export default function AddUser() {
+    const firstName = useRef();
+    const lastName = useRef();
+    const age = useRef();
+    const country = useRef();
+    
+    const _onSubmitBtnClick = function() {
+            const firstNameVal = firstName.current.value;
+            const lastNameVal = lastName.current.value;
+            const ageVal = age.current.value;
+            const countryVal = country.current.value;
+
+            props._onAddUserEvent ({
+                firstName: firstNameVal, 
+                lastName: lastNameVal, 
+                age: ageVal, 
+                country: countryVal
+            });
+
+            firstName.current.value = "";
+            lastName.current.value = "";
+            age.current.value = "";
+            country.current.value = "";
+    };
+
     return (
         <>
-            <h2>Add user in components</h2><hr />
+            <h2>Add User</h2><hr />
+            <form>
+                <div>
+                    <label>First Name :</label>
+                    <input type="text" name="firstName" placeholder="Please enter first name" ref={firstName}/>
+                </div>
+                <div>
+                    <label>Last Name :</label>
+                    <input type="text" name="lastName" placeholder="Please enter last name" ref={lastName}/>
+                </div>
+                <div>
+                    <label>Age :</label>
+                    <input type="text" name="age" placeholder="Please enter age" ref={age}/>
+                </div>
+                <div>
+                    <label>Country :</label>
+                    <input type="text" name="country" placeholder="Please enter country name" ref={country}/>
+                </div>
+                <button type="button" onClick={_onSubmitBtnClick}>Add User</button>
+            </form>
         </>
     )
-}
+} 
