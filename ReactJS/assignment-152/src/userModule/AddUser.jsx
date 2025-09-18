801a9144971553e295e8e03346474a5158d9f499
handrakantnb18 <bhosale.chandrakant1806@gmail.com>
2025-09-18 21:05:16 +0530

    updated successfully

--- ReactJS/assignment-152/src/userModule/AddUser.jsx
+++ ReactJS/assignment-152/src/userModule/AddUser.jsx
@@ -32,21 +32,43 @@ export default function AddUser(props) {
             <form>
                 <div>
                     <label>First Name :</label>
-                    <input type="text" name="firstName" placeholder="Please enter first name" ref={firstName}/>
+                    <input 
+                        type="text"
+                        name="firstName" 
+                        placeholder="Please enter first name" 
+                        ref={firstName}
+                        />
                 </div>
                 <div>
                     <label>Last Name :</label>
-                    <input type="text" name="lastName" placeholder="Please enter last name" ref={lastName}/>
+                    <input 
+                        type="text" 
+                        name="lastName" 
+                        placeholder="Please enter last name" 
+                        ref={lastName}
+                        />
                 </div>
                 <div>
                     <label>Age :</label>
-                    <input type="text" name="age" placeholder="Please enter age" ref={age}/>
+                    <input 
+                        type="text" 
+                        name="age" 
+                        placeholder="Please enter age" 
+                        ref={age}
+                        />
                 </div>
                 <div>
                     <label>Country :</label>
-                    <input type="text" name="country" placeholder="Please enter country name" ref={country}/>
+                    <input 
+                        type="text" 
+                        name="country" 
+                        placeholder="Please enter country name" 
+                        ref={country}
+                        />
                 </div>
-                <button type="button" onClick={_onSubmitBtnClick}>Add User</button>
+                <button type="button" onClick={_onSubmitBtnClick}>
+                    Add User
+                    </button>
             </form>
         </>
     )
